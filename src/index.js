ed1a58e8be6c6045c0e78aeddfc1528099faefc7
Jani Anttonen <jantto@utu.fi>
2018-03-07 22:16:38 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+/**
+ * VERY reusable React component.
+ * Extends HTML select so that it can better handle asynchronically loaded data.
+ */
+class Select extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: this.props.defaultValue
+    };
+  }
+  handleChange(e) {
+    e.preventDefault();
+    this.props.onChange(e);
+    this.setState({
+      selected: e.target.value
+    });
+  }
+  render() {
+    return (
+      <select
+        className="mobile-dropdown"
+        value={this.state.selected}
+        onChange={e => this.handleChange(e)}
+      >
+        {this.props.unselected && (
+          <option value={this.props.unselected.value}>
+            {this.props.unselected.label}
+          </option>
+        )}
+        {this.props.options &&
+          this.props.options.map((option, index) => (
+            <option key={`option${index}`} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+      </select>
+    );
+  }
+}
+
+export default Select;
